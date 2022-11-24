96bfaef2148669d6e7886020921744dadda4b237
Sergio Sixi <sergio.sixi@globant.com>
2022-11-24 13:23:29 -0500

    Revert "feat: pm2 monit file added"
    This reverts commit 1b84377024c13b73c8738913bdb03de28975ef54.

--- dist/domain/dto/customer-response.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CustomerResponseDto = void 0;
-class CustomerResponseDto {
-    customer;
-}
-exports.CustomerResponseDto = CustomerResponseDto;
-//# sourceMappingURL=customer-response.dto.js.map
